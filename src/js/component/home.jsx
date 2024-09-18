33553b3dc203b762739680967145ac00c62e109c
Yaralisa <yaralisa28@gmail.com>
2024-09-18 16:35:06 +0000

    proyectototolist terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 //include images into your bundle
 
 import ToDoAPI from "./TodoList..jsx";
+import OseaVisualTodDo from "./userGayer.jsx";
 
 //create your first component
 const Home = () => {
   return (
     <div className="text-center">
-      <ToDoAPI />
+      {/* <ToDoAPI /> */}
+      <OseaVisualTodDo/>
     </div>
   );
 };
